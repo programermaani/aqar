dd2b52a7c82339d32b4332f3429086a3299cd765
2te2st232 <2te2st232@gmail.com>
2025-05-31 14:26:31 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+  proxy: {
+    '/api': {
+      target: 'https://firebasestorage.googleapis.com',
+      changeOrigin: true,
+      rewrite: path => path.replace(/^\/api/, '')
+    }
+  }
+},
 });
+
